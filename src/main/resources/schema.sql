0a0c933b5963912edf558fd98bb10ee66a75c024
Ricardo Pina Arellano <ricardo.pina@adesis.com>
2016-12-08 20:55:06 -0600

    chore(BaaS): modified schema relates on the new changes

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS ISSUE;
 
 CREATE TABLE ISSUE (
-  id int(11) NOT NULL AUTO_INCREMENT,
+  id varchar(36) NOT NULL,
   title varchar(100) NOT NULL,
   description varchar(100) NOT NULL,
   type varchar(10) DEFAULT NULL,
@@ -12,6 +12,6 @@ CREATE TABLE ISSUE (
   PRIMARY KEY (id)
 );
 
-insert into ISSUE(title, description, type, priority, status, register_date, change_date) values('Service Error', 'Runtime error on service layer', 'BUG', 'MEDIUM', 'IN_PROGRESS', '2016-11-28 15:16:14', '2016-11-28 15:16:14');
-insert into ISSUE(title, description, type, priority, status, register_date, change_date) values('Logger Error', 'Runtime error on logger layer', 'BUG', 'HIGH', 'IN_PROGRESS', '2016-11-28 15:16:14', '2016-11-28 15:16:14');
-insert into ISSUE(title, description, type, priority, status, register_date, change_date) values('Core Error', 'Runtime error on core layer', 'BUG', 'HIGH', 'OPEN', '2016-11-28 15:16:14', '2016-11-28 15:16:14');
+insert into ISSUE(id, title, description, type, priority, status, register_date, change_date) values('18b526ab-8021-11e6-ae2a-42010af00004', 'Service Error', 'Runtime error on service layer', 'BUG', 'MEDIUM', 'IN_PROGRESS', '2016-11-28 15:16:14', '2016-11-28 15:16:14');
+insert into ISSUE(id, title, description, type, priority, status, register_date, change_date) values('18b526ab-8021-11e6-ae2a-45010af00004', 'Logger Error', 'Runtime error on logger layer', 'BUG', 'HIGH', 'IN_PROGRESS', '2016-11-28 15:16:14', '2016-11-28 15:16:14');
+insert into ISSUE(id, title, description, type, priority, status, register_date, change_date) values('18b526ab-8021-11e6-ae2a-46010af00004', 'Core Error', 'Runtime error on core layer', 'BUG', 'HIGH', 'OPEN', '2016-11-28 15:16:14', '2016-11-28 15:16:14');
