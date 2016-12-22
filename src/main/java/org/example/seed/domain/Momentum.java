6ac1ae8b4da4963e4fb43afc38675b04703e4082
Ricardo Pina Arellano <ricardo.pina@adesis.com>
2016-12-22 11:27:10 -0600

    feat(Event): add @XMLRootElement over event classes and put the total private attribute over CatalogIssueEvent

--- src/main/java/org/example/seed/domain/Momentum.java
+++ src/main/java/org/example/seed/domain/Momentum.java
@@ -2,12 +2,14 @@ package org.example.seed.domain;
 
 import lombok.Data;
 
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Date;
 
 /**
  * Created by Ricardo Pina Arellano on 24/11/2016.
  */
 @Data
+@XmlRootElement
 public class Momentum {
     private Date registerDate;
     private Date changeDate;
