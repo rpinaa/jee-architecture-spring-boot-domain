754caa9013ee4aef864b228964b5100479d8337a
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-04-27 01:14:31 -0500

    feat(Mapper): put MyBatis annotations into Mapper class

--- src/main/java/org/example/seed/mapper/IssueMapper.java
+++ src/main/java/org/example/seed/mapper/IssueMapper.java
@@ -1,6 +1,6 @@
 package org.example.seed.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.session.RowBounds;
 import org.example.seed.domain.Issue;
 import org.example.seed.event.issue.CreateIssueEvent;
@@ -16,15 +16,31 @@ import java.util.List;
 @Mapper
 public interface IssueMapper {
 
+    @Insert("INSERT INTO ISSUE" +
+            "(id, title, description, type, priority, status, register_date, change_date)" +
+            "VALUES(#{issue.id}, #{issue.title}, #{issue.description}, #{issue.type}, " +
+            "#{issue.priority}, #{issue.status}, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)")
+    @Options(useGeneratedKeys = true)
     int createIssue(final CreateIssueEvent createIssueEvent);
 
+    @Select("SELECT COUNT(*) FROM ISSUE")
     long countAllIssues();
 
+    @Select("SELECT * FROM ISSUE")
     List<Issue> findAllIssues(final RowBounds rb);
 
-    Issue findIssueById(final RequestIssueEvent requestIssueEvent);
+    @Select("SELECT * FROM ISSUE WHERE id = #{requestIssueEvent.id}")
+    Issue findIssueById(@Param("requestIssueEvent") final RequestIssueEvent requestIssueEvent);
 
+    @Update("UPDATE ISSUE SET" +
+            "description = #{issue.description}, " +
+            "type = #{issue.type}, " +
+            "priority = #{issue.priority}, " +
+            "status = #{issue.status}, " +
+            "change_date = CURRENT_TIMESTAMP" +
+            "WHERE id = #{issue.id, javaType=java.util.UUID}")
     int saveIssue(final UpdateIssueEvent updateIssueEvent);
 
+    @Delete("DELETE FROM ISSUE WHERE id = #{id, javaType=java.util.UUID}")
     int deleteIssue(final DeleteIssueEvent id);
 }
