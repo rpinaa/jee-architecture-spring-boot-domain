f90006d8b16d04077511f1346a4db7a7d8689d26
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-06-16 13:46:19 -0500

    feat(mapper): change new functionality for updateChef and updateAccount

--- src/main/java/org/example/seed/mapper/AccountMapper.java
+++ src/main/java/org/example/seed/mapper/AccountMapper.java
@@ -3,6 +3,8 @@ package org.example.seed.mapper;
 import org.apache.ibatis.annotations.*;
 import org.example.seed.event.chef.CreateChefEvent;
 import org.example.seed.event.chef.DeleteChefEvent;
+import org.example.seed.event.chef.RequestChefEvent;
+import org.example.seed.event.chef.UpdateChefEvent;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -22,7 +24,15 @@ public interface AccountMapper {
     int createAccount(final CreateChefEvent createChefEvent);
 
     @Select("SELECT account.id FROM account WHERE account.id IN (SELECT chef.fk_id_account FROM chef WHERE chef.id = #{id}) AND deleted = 0")
-    UUID findAccountId(final DeleteChefEvent deleteChefEvent);
+    UUID findAccount(final RequestChefEvent requestChefEvent);
+
+    @Update("UPDATE account SET" +
+            "first_name = #{chef.account.firstName}, " +
+            "last_name = #{chef.account.lastName}, " +
+            "email = #{chef.account.email}," +
+            "change_date = CURRENT_TIMESTAMP" +
+            "WHERE account.id = #{chef.account.id}")
+    int updateAccount(final UpdateChefEvent updateChefEvent);
 
     @Update("UPDATE account SET deleted = 1, change_date = CURRENT_TIMESTAMP WHERE account.id = #{id}")
     int deleteAccount(final UUID id);
