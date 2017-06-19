4e1fb3291497c16f1b9f4e585fbe93fcff7a63fb
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-06-18 23:00:56 -0500

    feat(mapper): update Account and Chef mapper to cover all business rules

--- src/main/java/org/example/seed/mapper/AccountMapper.java
+++ src/main/java/org/example/seed/mapper/AccountMapper.java
@@ -1,10 +1,9 @@
 package org.example.seed.mapper;
 
 import org.apache.ibatis.annotations.*;
+import org.example.seed.domain.Account;
+import org.example.seed.event.account.UpdateAccountEvent;
 import org.example.seed.event.chef.CreateChefEvent;
-import org.example.seed.event.chef.DeleteChefEvent;
-import org.example.seed.event.chef.RequestChefEvent;
-import org.example.seed.event.chef.UpdateChefEvent;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -16,23 +15,17 @@ import java.util.UUID;
 @Repository
 public interface AccountMapper {
 
-    @Insert("INSERT INTO account " +
-            "(id, first_name, last_name, email, secret, deleted, create_date, change_date)" +
-            "VALUES(#{chef.account.id}, #{chef.account.firstName}, #{chef.account.lastName}, " +
-            "#{chef.account.email}, NULL, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)")
+    @Insert("INSERT INTO account (id, first_name, last_name, email, secret, deleted, create_date, change_date)" +
+            "VALUES(#{chef.account.id}, #{chef.account.firstName}, #{chef.account.lastName}, #{chef.account.email}, NULL, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)")
     @Options(useGeneratedKeys = true)
     int createAccount(final CreateChefEvent chefEvent);
 
-    @Select("SELECT account.id FROM account WHERE account.id IN (SELECT chef.fk_id_account FROM chef WHERE chef.id = #{id}) AND deleted = 0")
-    UUID findAccount(final RequestChefEvent chefEvent);
+    @Select("SELECT * FROM account WHERE account.id = #{id} AND deleted = 0")
+    Account findAccount(final String id);
 
-    @Update("UPDATE account SET" +
-            "first_name = #{chef.account.firstName}, " +
-            "last_name = #{chef.account.lastName}, " +
-            "email = #{chef.account.email}," +
-            "change_date = CURRENT_TIMESTAMP" +
-            "WHERE account.id = #{chef.account.id}")
-    int updateAccount(final UpdateChefEvent chefEvent);
+    @Update("UPDATE account SET first_name = #{chef.account.firstName}, last_name = #{chef.account.lastName}, email = #{chef.account.email}, change_date = CURRENT_TIMESTAMP" +
+            "WHERE account.id = #{chef.account.id} AND deleted = 0")
+    int updateAccount(final UpdateAccountEvent accountEvent);
 
     @Update("UPDATE account SET deleted = 1, change_date = CURRENT_TIMESTAMP WHERE account.id = #{id}")
     int deleteAccount(final UUID id);
--- src/main/java/org/example/seed/mapper/ChefMapper.java
+++ src/main/java/org/example/seed/mapper/ChefMapper.java
@@ -2,7 +2,6 @@ package org.example.seed.mapper;
 
 import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.session.RowBounds;
-import org.example.seed.domain.Account;
 import org.example.seed.domain.Chef;
 import org.example.seed.event.chef.CreateChefEvent;
 import org.example.seed.event.chef.DeleteChefEvent;
@@ -11,6 +10,7 @@ import org.example.seed.event.chef.UpdateChefEvent;
 import org.springframework.stereotype.Repository;
 
 import java.util.Set;
+import java.util.UUID;
 
 /**
  * Created by PINA on 15/06/2017.
@@ -19,36 +19,30 @@ import java.util.Set;
 @Repository
 public interface ChefMapper {
 
-    @Insert("INSERT INTO chef" +
-            "(id, status, rfc, curp, rating, create_date, change_date, deleted, fk_id_account)" +
-            "VALUES(#{chef.id}, #{chef.status}, #{chef.rfc}, #{chef.curp}, #{chef.rating}," +
-            "CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0, #{chef.account.id})")
+    @Insert("INSERT INTO chef (id, status, rfc, curp, rating, create_date, change_date, deleted, fk_id_account)" +
+            "VALUES(#{chef.id}, #{chef.status}, #{chef.rfc}, #{chef.curp}, #{chef.rating}, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0, #{chef.account.id})")
     @Options(useGeneratedKeys = true)
     int createChef(final CreateChefEvent chefEvent);
 
-    @Select("SELECT COUNT(*) FROM chef")
+    @Select("SELECT COUNT(*) FROM chef WHERE deleted = 0")
     long countChefs();
 
-    @Select("SELECT * FROM chef")
+    @Select("SELECT * FROM chef WHERE deleted = 0")
     @Results(value = {
-            @Result(property = "account", column = "fk_id_account", one = @One(select = "findAccount"))
+            @Result(property = "account", column = "fk_id_account", one = @One(select = "org.example.seed.mapper.AccountMapper.findAccount"))
     })
     Set<Chef> findChefs(final RowBounds rb);
 
-    @Select("SELECT * FROM chef WHERE chef.id = #{id}")
+    @Select("SELECT * FROM chef WHERE chef.id = #{id} AND deleted = 0")
     @Results(value = {
-            @Result(property = "account", column = "fk_id_account", one = @One(select = "findAccount"))
+            @Result(property = "account", column = "fk_id_account", one = @One(select = "org.example.seed.mapper.AccountMapper.findAccount"))
     })
     Chef findChef(final RequestChefEvent chefEvent);
 
-    @Select("SELECT * FROM account WHERE account.id = #{id}")
-    Account findAccount(final String id);
+    @Select("SELECT chef.fk_id_account FROM chef WHERE chef.id = #{id} AND deleted = 0")
+    UUID findAccountUUID(final UUID id);
 
-    @Update("UPDATE chef SET" +
-            "rfc = #{chef.rfc}," +
-            "curp = #{chef.curp}," +
-            "change_date = CURRENT_TIMESTAMP" +
-            "WHERE chef.id = #{chef.id}")
+    @Update("UPDATE chef SET rfc = #{chef.rfc}, curp = #{chef.curp}, change_date = CURRENT_TIMESTAMP WHERE chef.id = #{chef.id} AND deleted = 0")
     int updateChef(final UpdateChefEvent chefEvent);
 
     @Update("UPDATE chef SET deleted = 1, change_date = CURRENT_TIMESTAMP WHERE chef.id = #{id}")
