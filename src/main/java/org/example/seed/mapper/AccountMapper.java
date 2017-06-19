dd6a45cc8150f1deee48094bdaee493580ada870
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-06-18 23:11:42 -0500

    style(mapper): change queries statements into MyBatis mappers

--- src/main/java/org/example/seed/mapper/AccountMapper.java
+++ src/main/java/org/example/seed/mapper/AccountMapper.java
@@ -16,17 +16,33 @@ import java.util.UUID;
 public interface AccountMapper {
 
     @Insert("INSERT INTO account (id, first_name, last_name, email, secret, deleted, create_date, change_date)" +
-            "VALUES(#{chef.account.id}, #{chef.account.firstName}, #{chef.account.lastName}, #{chef.account.email}, NULL, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)")
+            "VALUES(" +
+                "#{chef.account.id}," +
+                "#{chef.account.firstName}," +
+                "#{chef.account.lastName}," +
+                "#{chef.account.email}," +
+                "NULL, 0," +
+                "CURRENT_TIMESTAMP," +
+                "CURRENT_TIMESTAMP" +
+            ")")
     @Options(useGeneratedKeys = true)
     int createAccount(final CreateChefEvent chefEvent);
 
     @Select("SELECT * FROM account WHERE account.id = #{id} AND deleted = 0")
     Account findAccount(final String id);
 
-    @Update("UPDATE account SET first_name = #{chef.account.firstName}, last_name = #{chef.account.lastName}, email = #{chef.account.email}, change_date = CURRENT_TIMESTAMP" +
-            "WHERE account.id = #{chef.account.id} AND deleted = 0")
+    @Update("UPDATE account SET" +
+                "first_name = #{chef.account.firstName}," +
+                "last_name = #{chef.account.lastName}," +
+                "email = #{chef.account.email}," +
+                "change_date = CURRENT_TIMESTAMP" +
+            "WHERE account.id = #{chef.account.id}" +
+            "AND deleted = 0")
     int updateAccount(final UpdateAccountEvent accountEvent);
 
-    @Update("UPDATE account SET deleted = 1, change_date = CURRENT_TIMESTAMP WHERE account.id = #{id}")
+    @Update("UPDATE account SET" +
+                "deleted = 1," +
+                "change_date = CURRENT_TIMESTAMP" +
+            "WHERE account.id = #{id}")
     int deleteAccount(final UUID id);
 }
