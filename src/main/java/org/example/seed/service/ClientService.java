7808c14f461744209bc28a6a0545635c4d864bd8
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-06-25 20:35:43 -0500

    style(service): change signature for event

--- src/main/java/org/example/seed/service/ClientService.java
+++ src/main/java/org/example/seed/service/ClientService.java
@@ -9,13 +9,13 @@ import java.util.concurrent.Future;
  */
 public interface ClientService {
 
-    Future<CatalogClientEvent> requestAllClients(final RequestAllClientEvent clientEvent);
+    Future<CatalogClientEvent> requestAllClients(final RequestAllClientEvent event);
 
-    Future<ResponseClientEvent> createClient(final CreateClientEvent clientEvent);
+    Future<ResponseClientEvent> createClient(final CreateClientEvent event);
 
-    Future<ResponseClientEvent> requestClient(final RequestClientEvent clientEvent);
+    Future<ResponseClientEvent> requestClient(final RequestClientEvent event);
 
-    Future<ResponseClientEvent> updateClient(final UpdateClientEvent clientEvent);
+    Future<ResponseClientEvent> updateClient(final UpdateClientEvent event);
 
-    Future<ResponseClientEvent> deleteClient(final DeleteClientEvent clientEvent);
+    Future<ResponseClientEvent> deleteClient(final DeleteClientEvent event);
 }
