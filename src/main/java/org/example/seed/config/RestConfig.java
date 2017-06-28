1e614c0472c6d002432b12ef441042ee780334e0
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-06-28 11:26:10 -0500

    feat(config): create etag implementation using Shallow approach

--- src/main/java/org/example/seed/config/RestConfig.java
+++ src/main/java/org/example/seed/config/RestConfig.java
@@ -1,17 +1,22 @@
 package org.example.seed.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.servlet.Filter;
 
 /**
  * Created by Ricardo Pina Arellano on 25/11/2016.
  */
 @Configuration
 @EnableWebMvc
-public class APIConfig extends WebMvcConfigurerAdapter {
+public class RestConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
@@ -20,5 +25,22 @@ public class APIConfig extends WebMvcConfigurerAdapter {
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("xml", MediaType.APPLICATION_XML)
                 .mediaType("json", MediaType.APPLICATION_JSON);
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        final FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+
+        filterRegistrationBean.setFilter(this.etagFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setName("etagFilter");
+        filterRegistrationBean.setOrder(1);
+
+        return filterRegistrationBean;
+    }
+
+    @Bean(name = "etagFilter")
+    public Filter etagFilter() {
+        return new ShallowEtagHeaderFilter();
     }
 }
