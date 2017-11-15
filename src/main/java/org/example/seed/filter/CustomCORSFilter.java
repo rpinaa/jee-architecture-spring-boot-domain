96f3360522e18acbf6c50378d40d3aedcd9759a5
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-11-14 23:01:04 -0600

    style(filter): modify identation

--- src/main/java/org/example/seed/filter/CustomCORSFilter.java
+++ src/main/java/org/example/seed/filter/CustomCORSFilter.java
@@ -1,40 +1,30 @@
 package org.example.seed.filter;
 
-import java.io.IOException;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 public class CustomCORSFilter implements Filter {
 
-	@Override
-	public void destroy() {
+  @Override
+  public void destroy() { }
 
-	}
+  @Override
+  public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
 
-	@Override
-	public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
+    final HttpServletResponse httpServletResponse = (HttpServletResponse) response;
 
-		final HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+    httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+    httpServletResponse.setHeader("Access-Control-Allow-Methods", "DELETE, GET, OPTIONS, PATCH, POST, PUT");
+    httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
+    httpServletResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
 
-		httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
-		httpServletResponse.setHeader("Access-Control-Allow-Methods", "DELETE, GET, OPTIONS, PATCH, POST, PUT");
-		httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
-		httpServletResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+    chain.doFilter(request, response);
+  }
 
-		chain.doFilter(request, response);
-	}
-
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-
-	}
+  @Override
+  public void init(final FilterConfig filterConfig) throws ServletException { }
 }	
