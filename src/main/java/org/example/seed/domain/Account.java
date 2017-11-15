1f7420ebc99402970660daa81df03f92145ff01f
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-11-14 22:57:38 -0600

    style(domain): update identation

--- src/main/java/org/example/seed/domain/Account.java
+++ src/main/java/org/example/seed/domain/Account.java
@@ -15,23 +15,23 @@ import java.util.UUID;
 @Data
 public class Account {
 
-    public Account() {
-        this.id = UUID.randomUUID();
-    }
+  public Account() {
+    this.id = UUID.randomUUID();
+  }
 
-    @NotNull(groups = {ChefUpdateGroup.class})
-    private UUID id;
+  @NotNull(groups = {ChefUpdateGroup.class})
+  private UUID id;
 
-    @Email(groups = {ChefCreateGroup.class, ChefUpdateGroup.class})
-    @Size(max = 45, groups = {ChefCreateGroup.class, ChefUpdateGroup.class})
-    @NotNull(groups = {ChefCreateGroup.class, ChefUpdateGroup.class})
-    private String email;
+  @Email(groups = {ChefCreateGroup.class, ChefUpdateGroup.class})
+  @Size(max = 45, groups = {ChefCreateGroup.class, ChefUpdateGroup.class})
+  @NotNull(groups = {ChefCreateGroup.class, ChefUpdateGroup.class})
+  private String email;
 
-    @Size(min = 2, max = 80, groups = {ChefCreateGroup.class, ChefUpdateGroup.class})
-    @NotNull(groups = {ChefCreateGroup.class, ChefUpdateGroup.class})
-    private String firstName;
+  @Size(min = 2, max = 80, groups = {ChefCreateGroup.class, ChefUpdateGroup.class})
+  @NotNull(groups = {ChefCreateGroup.class, ChefUpdateGroup.class})
+  private String firstName;
 
-    @Size(min = 2, max = 80, groups = {ChefCreateGroup.class, ChefUpdateGroup.class})
-    @NotNull(groups = {ChefCreateGroup.class, ChefUpdateGroup.class})
-    private String lastName;
+  @Size(min = 2, max = 80, groups = {ChefCreateGroup.class, ChefUpdateGroup.class})
+  @NotNull(groups = {ChefCreateGroup.class, ChefUpdateGroup.class})
+  private String lastName;
 }
